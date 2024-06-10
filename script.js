35f2ad8204cca70eaeeccf77d521f24362464563
wonjangcloud9 <venuscwj@naver.com>
2024-06-10 10:59:04 +0900

    ✨ OrthographicCamera

--- script.js
+++ script.js
@@ -51,7 +51,16 @@ scene.add(mesh);
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+const aspectRatio = sizes.width / sizes.height;
+const camera = new THREE.OrthographicCamera(
+  -1 * aspectRatio,
+  1 * aspectRatio,
+  1,
+  -1,
+  0.1,
+  100
+);
 camera.position.x = 2;
 camera.position.y = 2;
 camera.position.z = 2;
