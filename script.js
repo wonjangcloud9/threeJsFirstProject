13d8da060fb3534f8f6e81098ef792fdb25db589
wonjangcloud9 <venuscwj@naver.com>
2024-06-10 10:32:33 +0900

    ⚡️ Setup for cameras

--- script.js
+++ script.js
@@ -4,37 +4,6 @@ import gsap from "gsap";
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
-// Scene
-const scene = new THREE.Scene();
-
-/**
- * Objects
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-// mesh.position.z = -3;
-// mesh.position.y = -2;
-// mesh.position.x = 3;
-
-mesh.position.set(0.7, -0.6, 1);
-
-scene.add(mesh);
-
-// Scale
-
-mesh.scale.set(2, 0.5, 0.5);
-
-// Rotation
-
-mesh.rotation.reorder("YXZ");
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
-
-// Axes helper
-const axesHelper = new THREE.AxesHelper();
-scene.add(axesHelper);
-
 /**
  * Sizes
  */
@@ -43,13 +12,50 @@ const sizes = {
   height: 600,
 };
 
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+scene.add(mesh);
+// const mesh = new THREE.Mesh(geometry, material);
+// mesh.position.z = -3;
+// mesh.position.y = -2;
+// mesh.position.x = 3;
+
+// mesh.position.set(0.7, -0.6, 1);
+
+// scene.add(mesh);
+
+// Scale
+
+// mesh.scale.set(2, 0.5, 0.5);
+
+// Rotation
+
+// mesh.rotation.reorder("YXZ");
+// mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.y = Math.PI * 0.25;
+
+// Axes helper
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
+
 /**
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
-// camera.position.y = 1;
-// camera.position.x = 1;
+camera.position.x = 2;
+camera.position.y = 2;
+camera.position.z = 2;
+camera.lookAt(mesh.position);
 scene.add(camera);
 
 /**
@@ -62,20 +68,15 @@ renderer.setSize(sizes.width, sizes.height);
 
 // Animation
 
-// const clock = new THREE.Clock();
+const clock = new THREE.Clock();
 
-gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+// gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
 
 const tick = () => {
-  // const elapsedTime = clock.getElapsedTime();
+  const elapsedTime = clock.getElapsedTime();
 
   // Update objects
-  // mesh.rotation.y = elapsedTime;
-
-  // Update camera
-  // camera.position.y = Math.sin(elapsedTime);
-  // camera.position.x = Math.cos(elapsedTime);
-  // camera.lookAt(mesh.position);
+  mesh.rotation.y = elapsedTime;
 
   // Render
   renderer.render(scene, camera);
